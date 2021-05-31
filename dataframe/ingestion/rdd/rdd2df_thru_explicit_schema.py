3f67afe39727c1da6262b6c988a9450af5ccd5a9
Rohith chava <chavarohith5@gmail.com>
2021-05-31 21:52:21 +0530

    initial commit

--- dataframe/ingestion/rdd/rdd2df_thru_explicit_schema.py
+++ dataframe/ingestion/rdd/rdd2df_thru_explicit_schema.py
@@ -11,8 +11,6 @@ if __name__ == '__main__':
     spark = SparkSession \
         .builder \
         .appName("DataFrames examples") \
-        .config('spark.jars.packages', 'org.apache.hadoop:hadoop-aws:2.7.4') \
-        .master('local[*]') \
         .getOrCreate()
 
     spark.sparkContext.setLogLevel("ERROR")
@@ -36,7 +34,7 @@ if __name__ == '__main__':
         .filter(lambda record: record.find("txn_id")) \
         .map(lambda record: record.split("|")) \
         .map(lambda record: Row(int(record[0]), int(record[1]), float(record[2]), int(record[3]), int(record[4]), int(record[5]), record[6]))
-        # RDD[Row[Long, Long, Double, Long, Int, Long, String]]
+        # RDD[Row[int, int, float, int, int, int, str]]
 
     # Creating the schema
     txn_fct_schema = StructType([
